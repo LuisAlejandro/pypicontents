4c64e3b27897a7e69917dc69bd56f484dd11f5ef
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-04-24 02:21:54 -0430

    [REF] Adding methods to access each setup.py and ask him directly which packages provides.

--- process.py
+++ process.py
@@ -1,66 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
+import re
+import sys
 import json
 import glob
+import shutil
 import tarfile
 import zipfile
 import urllib2
-from xmlrpclib import ServerProxy
+import setuptools
+import distutils
 
-jsondict ={}
-cachedir = os.environ['HOME']+'/.cache/pip/'
+from xmlrpclib import ServerProxy
+from pkg_resources import parse_version
+
+def dummysetup(*args, **kwargs):
+    global setupargs
+    setupargs = kwargs
+
+setuptools.setup = dummysetup
+distutils.core.setup = dummysetup
+
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+pypijson = os.path.join(scriptdir, 'pypi-contents.json')
+cachedir = os.path.join(os.environ['HOME'], '.cache', 'pip')
+
+with open(pypijson, 'r') as jsonfile:
+    jsondict = json.loads(jsonfile.read())
 
 pypi = ServerProxy('https://pypi.python.org/pypi')
 
 for pkgname in pypi.list_packages():
-    pkgreleases = sorted(pypi.package_releases(pkgname))
+
+    if not pkgname in jsondict:
+        jsondict[pkgname] = {'version':[''],
+                             'modules':[''],
+                             'scripts':[''],
+                             'contents':['']}
+
+    pkgreleases = pypi.package_releases(pkgname)
 
     if pkgreleases:
-        pkgversion = pkgreleases[0]
+        pkgreleases = [parse_version(v) for v in pkgreleases]
+        pkgversion = str(sorted(pkgreleases)[-1])
+        oldver = jsondict[pkgname]['version'][0]
 
-        for pkgtar in pypi.release_urls(pkgname, pkgversion):
-            if pkgtar['python_version'] == 'source':
-                arurl = pkgtar['url']
+        if oldver != pkgversion:
+
+            arurl = ''
+            for pkgtar in pypi.release_urls(pkgname, pkgversion):
+                if pkgtar['python_version'] == 'source':
+                    arurl = pkgtar['url']
+                    break
+
+            if arurl:
+                print 'Updating package "%s" (%s > %s) ...' % (pkgname, oldver,
+                                                               pkgversion)
                 arname = os.path.basename(arurl)
-                arpath = cachedir+arname
+                arpath = os.path.join(cachedir, arname)
 
-                if not os.path.isfile(arpath):
-                    for oldar in glob.glob(cachedir+pkgname+'-*'):
-                        os.remove(oldar)
+                ardownobj = urllib2.urlopen(arurl)
 
-                    print 'Downloading %s ...' % arname
-                    ardownobj = urllib2.urlopen(arurl)
-                    arfileobj = open(arpath, 'w')
+                with open(arpath, 'w') as arfileobj:
                     arfileobj.write(ardownobj.read())
-                    arfileobj.close()
 
                 if arurl.endswith('.zip'):
+                    armode = 'r'
                     archive_open = zipfile.ZipFile
                     zipfile.ZipFile.list = zipfile.ZipFile.namelist
-                    zipfile.ZipFile.openfile = zipfile.ZipFile.open
 
                 elif arurl.endswith('.tar.gz'):
+                    armode = 'r:gz'
                     archive_open = tarfile.open
                     tarfile.TarFile.list = tarfile.TarFile.getnames
-                    tarfile.TarFile.openfile = tarfile.TarFile.extractfile
 
-                cmp = archive_open(arpath, 'r')
+                elif arurl.endswith('.tar.bz2'):
+                    armode = 'r:bz2'
+                    archive_open = tarfile.open
+                    tarfile.TarFile.list = tarfile.TarFile.getnames
+
+                else:
+                    print 'NO SE QUE ES: '+arurl
+
+                cmp = archive_open(arpath, armode)
+                cmp.extractall(cachedir)
                 cmplist = cmp.list()
-                jsondict[pkgname] = {'contents': cmplist}
-
-                for f in cmplist:
-                    fdirname = os.path.dirname(f)
-                    fbasename = os.path.basename(f)
-
-                    if (fdirname.endswith('.egg-info') and
-                        fbasename == 'top_level.txt'):
-                        modulesobj = cmp.openfile(f)
-                        pkgmodules = modulesobj.read()
-                        jsondict[pkgname].update(
-                            {'modules': filter(None, pkgmodules.split('\n'))})
-                        modulesobj.close()
                 cmp.close()
 
-jsonfileobj = open('pypi-contents.json', 'w')
-jsonfileobj.write(json.dumps(jsondict))
-jsonfileobj.close()
+                pkgdir = os.path.normpath(cmplist[0]).split(os.sep)[0]
+                pkgpath = os.path.join(cachedir, pkgdir)
+                setuppath = os.path.join(pkgpath, 'setup.py')
+
+                with open(setuppath, 'r') as setuppy:
+                    setuppyconts = setuppy.read()
+
+                os.chdir(pkgpath)
+                exec setuppyconts
+
+                if 'py_modules' in setupargs:
+                    jsondict[pkgname]['modules'] = setupargs['py_modules']
+
+                if 'packages' in setupargs:
+                    if setupargs['packages'][0]:
+                        jsondict[pkgname]['modules'] = setupargs['packages']
+                    else:
+                        pys = glob.glob(os.path.join(pkgpath, '*.py'))
+                        pys = [os.path.splitext(os.path.basename(p))[0] for p in pys if
+                               os.path.basename(p) != 'setup.py']
+                        jsondict[pkgname]['modules'] = pys
+
+                if 'scripts' in setupargs:
+                    jsondict[pkgname]['scripts'] = setupargs['scripts']
+
+                jsondict[pkgname]['contents'] = cmplist
+                jsondict[pkgname]['version'][0] = pkgversion
+
+                shutil.rmtree(pkgpath)
+                os.remove(arpath)
+
+with open(pypijson, 'w') as jsonfileobj:
+    jsonfileobj.write(json.dumps(jsondict, indent=4,
+                                 sort_keys=True, separators=(',', ': ')))
 

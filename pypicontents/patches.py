1d9a5eebb37eb79d44a01763c1b104df37cd059f
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-05-13 16:18:03 -0400

    [REF] Implementing a better module mocking.

--- pypicontents/patches.py
+++ pypicontents/patches.py
@@ -1,69 +1,59 @@
 # -*- coding: utf-8 -*-
 
 import os
-import codecs
 import sys
-import setuptools
-import distutils
-
-from io import StringIO
-from types import ModuleType
+import imp
 
 
-class false_module(ModuleType):
-    def __getattr__(self, name):
-        print(name)
+def false_import(name, globals=None, locals=None, fromlist=None, level=None):
+    class false_module(object):
+        def __init__(self, *args, **kwargs):
+            print args, kwargs, 'init'
+        def __dict__(self, *args, **kwargs):
+            print args, kwargs, 'dict'
+        def __call__(self, *args, **kwargs):
+            print args, kwargs, 'call'
+        def __getattr__(self, name):
+            def method(*args, **kwargs):
+                print args, kwargs, 'getattr'
+            return method
 
+    def false_setup(*args, **kwargs):
+        global setupargs
+        setupargs = kwargs
 
-def false_setup(*args, **kwargs):
-    global setupargs
-    setupargs = kwargs
+    def false_find_module(name):
+        try:
+            fp, pathname, description = imp.find_module(name)
+        except ImportError:
+            return false_module
+        else:
+            return imp.load_module(name, fp, pathname, description)
 
+    if name == 'setuptools' or name == 'distutils.core':
+        if 'setup' in fromlist:
+            mod = false_find_module(name)
+            mod.setup = false_setup
+            return mod
 
-def false_exit(*args, **kwargs):
-    print('[WARNING] This package tried to exit.')
+    elif name in sys.modules:
+        return sys.modules[name]
+    else:
+        return false_find_module(name)
 
 
 def patchedglobals(setuppath):
-
-    modules_to_patch = ['distribute_setup',
-                        'Cython.build',
-                        'Cython.Build',
-                        'Cython.Distutils',
-                        'pypandoc',
-                        'numpy',
-                        'numpy.distutils',
-                        'scipy.weave',
-                        'ldap3',
-                        'yaml',
-                        'arrayfire',
-                        '_thread',
-                        'django.utils',
-                        'pip.req']
-
-    os._exit = false_exit
-    sys.exit = false_exit
-    setuptools.setup = false_setup
-    distutils.core.setup = false_setup
 
     env = globals()
     env.update({
         '__file__': setuppath,
         '__name__': '__main__',
         '__package__': None,
-        'os._exit': os._exit,
-        'sys.exit': sys.exit,
-        'setuptools.setup': setuptools.setup,
-        'distutils.core.setup': distutils.core.setup,
     })
 
     env['__builtins__'].update({
-        'exit': false_exit
+        '__import__': false_import
     })
-
-
-    for m in modules_to_patch:
-        env[m] = false_module
 
     return env
 

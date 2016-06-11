9704913c712c622b88692b23750078160ca1e384
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-06-11 15:59:17 -0400

    [REF] Refactoring import procedure to cover more failing packages.

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
 
 import os
 import sys
-import errno
 
 from pypicontents.process import process
-from pypicontents.patches import patchedglobals
-from pypicontents.utils import create_file_from_setup, s, u
 
 if __name__ == '__main__':
 
-    if len(sys.argv) > 1:
-        setuppath = sys.argv[1]
-        pkgpath = os.path.dirname(setuppath)
-
-        env = patchedglobals()
-        env.update({'__file__': setuppath})
-
-        os.chdir(pkgpath)
-        sys.path.append(pkgpath)
-
-        while True:
-            try:
-                with open(setuppath) as setup:
-                    exec(compile(s(setup.read()), setuppath, 'exec'), env)
-            except BaseException as e:
-                if type(e) == IOError or type(e) == OSError:
-                    if hasattr(e, 'errno') and hasattr(e, 'filename'):
-                        if e.errno == errno.ENOENT:
-                            create_file_from_setup(setuppath, e.filename)
-                else:
-                    sys.stderr.write(str(e))
-                    break
-            else:
-                break
-    else:
-        try:
-            process(os.environ.get('PYPICONTENTSRANGE', '0-z'))
-        except KeyboardInterrupt:
-            sys.exit('Execution interrupted by user.')
+    try:
+        process(os.environ.get('PYPICONTENTSRANGE', '0-z'))
+    except KeyboardInterrupt:
+        sys.exit('Execution interrupted by user.')
---
+++ wrapper.py
@@ -0,0 +1,23 @@
+
+import os
+import sys
+
+from pypicontents.patches import patchedglobals
+from pypicontents.utils import s
+
+if __name__ == '__main__':
+
+    setuppath = sys.argv[1]
+    pkgpath = os.path.dirname(setuppath)
+
+    env = patchedglobals()
+    env.update({'__file__': setuppath})
+
+    os.chdir(pkgpath)
+    sys.path.append(pkgpath)
+
+    try:
+        with open(setuppath) as _sfile:
+            exec(s(_sfile.read()), env)
+    except BaseException as e:
+        sys.stderr.write(str(e))

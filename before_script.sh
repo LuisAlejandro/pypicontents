abccb80a53cf1f2198c50091e3b19e67a3257921
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-11-30 17:33:23 -0400

    Including dockerfiles. Moving scripts to separate package: pypirazzi.

--- before_script.sh
+++ before_script.sh
@@ -3,14 +3,12 @@
 set -ev
 
 if [ -n "${PYPICONTENTSRANGE}" ]; then
-	docker pull luisalejandro/python:sid
 	git fetch origin contents:contents
 	git checkout master
 	git checkout contents data/${PYPICONTENTSRANGE}/pypi.json logs/${PYPICONTENTSRANGE}/pypi.log
 fi
 
 if [ -n "${STDLIBCONTENTS}" ]; then
-	docker pull luisalejandro/python:sid
 	git fetch origin contents:contents
 	git checkout master
 	git checkout contents stdlib/${STDLIBCONTENTS}/stdlib.json

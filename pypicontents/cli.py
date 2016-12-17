c1b0d30ad29096e500a1952cf040cd434a894c59
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-12-17 01:31:27 -0400

    [REF] Improving documentation. [REF] Improving commandline parser.

--- pypicontents/cli.py
+++ pypicontents/cli.py
@@ -25,10 +25,12 @@ parameters, show help, version and controls the logging level.
 """
 from __future__ import absolute_import, print_function
 
+import os
 from argparse import ArgumentParser
 
 from . import __version__, __description__
 from .core.logger import logger
+from .core.utils import create_file_if_notfound
 from .api.pypi import main as pypi
 from .api.stdlib import main as stdlib
 from .api.merge import main as merge
@@ -51,84 +53,152 @@ def commandline(argv=None):
     """
     assert isinstance(argv, (list, type(None)))
 
-    parser = ArgumentParser(description=__description__)
-    parser.add_argument(
+    parser = ArgumentParser(
+        prog='pypicontents', description=__description__, add_help=False,
+        usage='\t%(prog)s [options]\n\t%(prog)s <command> [options]')
+    gen_options = parser.add_argument_group('General Options')
+    gen_options.add_argument(
         '-V', '--version', action='version',
         version='pypicontents {:s}'.format(__version__),
         help='Print version and exit.')
-    subparsers = parser.add_subparsers(title='commands')
+    gen_options.add_argument(
+        '-h', '--help', action='help', help='Show this help message and exit.')
+    subparsers = parser.add_subparsers(title='Commands', metavar='')
 
     # pypi command
-    pypi_parser = subparsers.add_parser('pypi')
+    pypi_parser = subparsers.add_parser(
+        'pypi', prog='pypicontents', usage='%(prog)s pypi [options]',
+        help='Generate a JSON Module Index from PyPI.', add_help=False)
     pypi_parser.set_defaults(command=pypi)
-    pypi_parser.add_argument(
+    pypi_gen_options = pypi_parser.add_argument_group('General Options')
+    pypi_gen_options.add_argument(
+        '-V', '--version', action='version',
+        version='pypicontents {:s}'.format(__version__),
+        help='Print version and exit.')
+    pypi_gen_options.add_argument(
+        '-h', '--help', action='help', help='Show this help message and exit.')
+    pypi_options = pypi_parser.add_argument_group('Pypi Options')
+    pypi_options.add_argument(
         '-l', '--loglevel', default='INFO', metavar='<level>',
         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
         help=('Logger verbosity level (default: INFO). Must be one of: '
               'DEBUG, INFO, WARNING, ERROR or CRITICAL.'))
-    pypi_parser.add_argument(
+    pypi_options.add_argument(
         '-f', '--logfile', metavar='<path>',
         help='A path pointing to a file to be used to store logs.')
-    pypi_parser.add_argument(
+    pypi_options.add_argument(
         '-o', '--outputfile', required=True, metavar='<path>',
         help=('A path pointing to a file that will be used to store the '
-              'JSON Module Index.'))
-    pypi_parser.add_argument(
+              'JSON Module Index (required).'))
+    pypi_options.add_argument(
         '-R', '--letter-range', default='0-z', metavar='<letter/number>',
         help=('An expression representing an alphanumeric range to be used to '
-              'filter packages from PyPI. You can use a single alphanumeric '
-              'character like "0" to process only packages beginning with '
-              '"0". You can use commas use as a list o dashes to use as an '
-              'interval.'))
-    pypi_parser.add_argument(
+              'filter packages from PyPI (default: 0-z). You can use a single '
+              'alphanumeric character like "0" to process only packages '
+              'beginning with "0". You can use commas use as a list o dashes '
+              'to use as an interval.'))
+    pypi_options.add_argument(
         '-L', '--limit-log-size', default='3M', metavar='<size>',
-        help='Stop processing if log size exceeds <size>.')
-    pypi_parser.add_argument(
+        help='Stop processing if log size exceeds <size> (default: 3M).')
+    pypi_options.add_argument(
         '-M', '--limit-mem', default='2G', metavar='<size>',
-        help='Stop processing if process memory exceeds <size>.')
-    pypi_parser.add_argument(
+        help='Stop processing if process memory exceeds <size> (default: 2G).')
+    pypi_options.add_argument(
         '-T', '--limit-time', default='2100', metavar='<sec>',
-        help='Stop processing if process time exceeds <sec>.')
+        help='Stop processing if process time exceeds <sec> (default: 2100).')
 
     # stdlib command
-    stdlib_parser = subparsers.add_parser('stdlib')
+    stdlib_parser = subparsers.add_parser(
+        'stdlib', prog='pypicontents', usage='%(prog)s stdlib [options]',
+        help=('Generate a JSON Module Index based on the Python '
+              'Standard Library.'), add_help=False)
     stdlib_parser.set_defaults(command=stdlib)
-    stdlib_parser.add_argument(
-        '-o', '--outputfile', required=True, metavar='',
-        help='')
-    stdlib_parser.add_argument(
-        '-p', '--pyver', default='2.7',
-        help='')
+    stdlib_gen_options = stdlib_parser.add_argument_group('General Options')
+    stdlib_gen_options.add_argument(
+        '-V', '--version', action='version',
+        version='pypicontents {:s}'.format(__version__),
+        help='Print version and exit.')
+    stdlib_gen_options.add_argument(
+        '-h', '--help', action='help', help='Show this help message and exit.')
+    stdlib_options = stdlib_parser.add_argument_group('Stdlib Options')
+    stdlib_options.add_argument(
+        '-o', '--outputfile', required=True, metavar='<path>',
+        help=('A path pointing to a file that will be used to store the '
+              'JSON Module Index (required).'))
+    stdlib_options.add_argument(
+        '-p', '--pyver', default='2.7', metavar='<version>',
+        choices=['2.6', '2.7', '3.2', '3.4', '3.5', '3.6'],
+        help=('Python version to be used for the Standard Library '
+              '(default: 2.7).'))
 
     # merge command
-    merge_parser = subparsers.add_parser('merge')
+    merge_parser = subparsers.add_parser(
+        'merge', prog='pypicontents', usage='%(prog)s merge [options]',
+        help='Merge JSON files generated by pypi or stdlib commands.',
+        add_help=False)
     merge_parser.set_defaults(command=merge)
-    merge_parser.add_argument(
-        '-i', '--inputdir', required=True, metavar='',
-        help='')
-    merge_parser.add_argument(
-        '-o', '--outputfile', required=True, metavar='',
-        help='')
+    merge_gen_options = merge_parser.add_argument_group('General Options')
+    merge_gen_options.add_argument(
+        '-V', '--version', action='version',
+        version='pypicontents {:s}'.format(__version__),
+        help='Print version and exit.')
+    merge_gen_options.add_argument(
+        '-h', '--help', action='help', help='Show this help message and exit.')
+    merge_options = merge_parser.add_argument_group('Merge Options')
+    merge_options.add_argument(
+        '-i', '--inputdir', required=True, metavar='<path>',
+        help=('A path pointing to a directory containing JSON files generated '
+              'by pypi or stdlib commands (required).'))
+    merge_options.add_argument(
+        '-o', '--outputfile', required=True, metavar='<path>',
+        help=('A path pointing to a file that will be used to store the '
+              'merged JSON files (required).'))
 
     # errors command
-    errors_parser = subparsers.add_parser('errors')
+    errors_parser = subparsers.add_parser(
+        'errors', prog='pypicontents', usage='%(prog)s errors [options]',
+        help='Extract errors from log files generated by the pypi command.',
+        add_help=False)
     errors_parser.set_defaults(command=errors)
-    errors_parser.add_argument(
-        '-i', '--inputdir', required=True, metavar='',
-        help='')
-    errors_parser.add_argument(
-        '-o', '--outputfile', required=True, metavar='',
-        help='')
+    errors_gen_options = errors_parser.add_argument_group('General Options')
+    errors_gen_options.add_argument(
+        '-V', '--version', action='version',
+        version='pypicontents {:s}'.format(__version__),
+        help='Print version and exit.')
+    errors_gen_options.add_argument(
+        '-h', '--help', action='help', help='Show this help message and exit.')
+    errors_options = errors_parser.add_argument_group('Errors Options')
+    errors_options.add_argument(
+        '-i', '--inputdir', required=True, metavar='<path>',
+        help=('A path pointing to a directory containing JSON files generated '
+              'by the pypi command (required).'))
+    errors_options.add_argument(
+        '-o', '--outputfile', required=True, metavar='<path>',
+        help=('A path pointing to a file that will be used to store the '
+              'errors (required).'))
 
     # stats command
-    stats_parser = subparsers.add_parser('stats')
+    stats_parser = subparsers.add_parser(
+        'stats', prog='pypicontents', usage='%(prog)s stats [options]',
+        help=('Extract statistics from log files generated by the '
+              'pypi command.'), add_help=False)
     stats_parser.set_defaults(command=stats)
-    stats_parser.add_argument(
-        '-i', '--inputdir', required=True, metavar='',
-        help='')
-    stats_parser.add_argument(
-        '-o', '--outputfile', required=True, metavar='',
-        help='')
+    stats_gen_options = stats_parser.add_argument_group('General Options')
+    stats_gen_options.add_argument(
+        '-V', '--version', action='version',
+        version='pypicontents {:s}'.format(__version__),
+        help='Print version and exit.')
+    stats_gen_options.add_argument(
+        '-h', '--help', action='help', help='Show this help message and exit.')
+    stats_options = stats_parser.add_argument_group('Stats Options')
+    stats_options.add_argument(
+        '-i', '--inputdir', required=True, metavar='<path>',
+        help=('A path pointing to a directory containing JSON files generated '
+              'by the pypi command (required).'))
+    stats_options.add_argument(
+        '-o', '--outputfile', required=True, metavar='<path>',
+        help=('A path pointing to a file that will be used to store the '
+              'statistics (required).'))
 
     return parser.parse_args(argv)
 
@@ -149,6 +219,13 @@ def main(argv=None):
     assert isinstance(argv, (list, type(None)))
 
     args = commandline(argv)
+
+    if args.command.__name__ == 'pypi':
+        if not os.path.isfile(args.logfile):
+            create_file_if_notfound(args.logfile)
+    else:
+        args.logfile = None
+        args.loglevel = 'INFO'
 
     logger.start(args.logfile)
     logger.loglevel(args.loglevel)

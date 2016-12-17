b9c4def79b2e73c3368baeae4c698e6e0018d52b
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-12-16 23:15:42 -0400

    [REF] Renaming commands. [REF] Improving documentation.

--- pypicontents/wrapper.py
+++ pypicontents/wrapper.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#   This file is part of PyPIContents.
+#   Copyright (C) 2016, PyPIContents Developers.
+#
+#   Please refer to AUTHORS.rst for a complete list of Copyright holders.
+#
+#   PyPIContents is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   PyPIContents is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see http://www.gnu.org/licenses.
 
 import os
 import sys
@@ -38,8 +56,8 @@ def s(x):
     return x.encode('utf-8')
 
 
-def false_import(name, globals={}, locals={},
-                 fromlist=[], level=default_import_level):
+def false_import(name, globals={}, locals={}, fromlist=[],
+                 level=default_import_level):
 
     class ImpostorModule(object):
         def __init__(self, *args, **kwargs):
